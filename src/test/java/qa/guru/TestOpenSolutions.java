10bae41227e4841edf342e1f8c94071e8cf1bb68
Ekaterina Zotova <karmengerl@gmail.com>
2024-01-08 12:50:10 +0300

    init commit Home work 6

---
+++ src/test/java/qa/guru/TestOpenSolutions.java
@@ -0,0 +1,22 @@
+package qa.guru;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestOpenSolutions {
+
+    @Test
+    void openSolutionsTest() {
+        Configuration.pageLoadStrategy = "eager";
+        open("https://github.com/");
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(".border-bottom").$(byText("Enterprise")).click();
+        $(".enterprise-lp").shouldHave(text("The AI-powered\n" +
+                "developer platform."));
+        //sleep(5000);
+    }
+}
